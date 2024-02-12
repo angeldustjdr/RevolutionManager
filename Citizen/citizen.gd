0d348a46f98a64453f20623da6177c2b1304f847
LI Simon <SL236797@intra.cea.fr>
2024-02-12 20:21:21 +0100

    'yeah'

---
+++ Citizen/citizen.gd
@@ -0,0 +1,9 @@
+class_name citizen
+
+var name : String = ""
+var satisfaction : float = 0.0 #Am I satisfied with my current situation (from -100 to 100)
+var spark : float = 0.0 #Am I willing to act to change my situation (from -100 to 100)
+
+var myMoralValuesScore : Dictionary = {}
+var listOfValues : Array = []
+
---
+++ Data/moral_values.gd
@@ -0,0 +1,18 @@
+class_name MoralValues
+
+# source : https://www.cairn.info/revue-francaise-de-sociologie-1-2006-4-page-929.htm
+
+var values : Dictionary = {
+	"Autonomy" : ["Creativity","Liberty","Self Determination","Curiosity","Independant"],
+	"Stimulation" : ["Fullfilling Life","Searching for new experiences","Adventures"],
+	"Hedonism" : ["Pleasure","Enjoying life","Laugthing with friends and family"],
+	"Sucess" : ["Ambition","Social recognition","Being idolized"],
+	"Power" : ["Authority","Wealth","High social status","Influence"],
+	"Security" : ["Social order","National security","Clean streets","Being in good health","Loyalty"],
+	"Conformity" : ["Autodiscipline","Politeness","Children obeying their parents","Respect of the elders"],
+	"Tradition" : ["Respect of traditions","Respect of religion","Spiritual elevation"],
+	"Kindness" : ["Honesty","Responsable people","Solidarity"],
+	"Universalism" : ["Justice","Social equility","World peace","Environment awareness"]
+}
+
+var labels : Array = ["Autonomy","Stimulation","Hedonism","Sucess","Power","Security","Conformity","Tradition","Kindness","Universalism"]
