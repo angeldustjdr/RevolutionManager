4a90e4d8be952dd5207ef89413bb12ba80d1b48c
Angeldust <simon.li@gadz.org>
2024-02-13 22:02:18 +0100

    generation occupation et income

--- Citizen/citizen.gd
+++ Citizen/citizen.gd
@@ -5,11 +5,10 @@ var satisfaction : float = 0.0 #Am I satisfied with my current situation (from -
 var spark : float = 0.0 #Am I willing to act to change my situation (from -100 to 100)
 
 var myMoralValuesScore : Dictionary = {}
-var listOfValues : Array = []
+# source : https://www.cairn.info/revue-francaise-de-sociologie-1-2006-4-page-929.htm
+var moralValues : Array = ["Autonomy","Stimulation","Hedonism","Sucess","Power","Security","Conformity/Tradition","Kindness","Universalism"]
 
 func initialize():
-	profile = {
-		"Name" : INSEE.pickName(),
-		"Age" : INSEE.pickAge()
-	}
+	profile = INSEE.createCitizen()
 	print(profile)
+
--- Data/insee.gd
+++ Data/insee.gd
@@ -45,24 +45,73 @@ var agePyramid : Dictionary = {
 	"ProbaCumul" : [17,23.3,29.1,34.6,40.5,46.7,53,59.1,65.7,72.2,78.4,84.1,89.5,100]
 }
 
-var job : Dictionary = {
-	"Agriculteur" : {
+var jobStats : Dictionary = {
+	"Farmer" : {
 		"ProbaCumul Niveau de vie" : [16.7, 27.2, 36.5, 43.7, 50.3, 57.8, 64.3, 72.1, 82.4, 100.0],
-		"RevenuParDecile" : [9140, 15125, 18117.5, 21110, 25210, 29310, 34488.3333, 39461.6667, 51820, 51820],
+		"RevenuParDecile" : [9140,13408,17676,21944,26212,30480,34748,39016,43284,47552,51820],
 		"Part dans la population" : 4.2
-	}
+	},
+	"Executive" : {
+		"ProbaCumul Niveau de vie" : [2.8,4.2,6.1,8.8,12.9,18.8,26.1,40.7,62.7,100],
+		"RevenuParDecile" : [19710,24006,28302,32598,36894,41190,45486,49782,54078,58374,62670],
+		"Part dans la population" : 10.4
+	},
+	"Intermediate occupation" : {
+		"ProbaCumul Niveau de vie" : [4.1,7.9,13.6,21.3,31.1,42.9,58,73.9,89.5,100],
+		"RevenuParDecile" : [15330,17770,20210,22650,25090,27530,29970,32410,34850,37290,39730],
+		"Part dans la population" : 14.6
+	},
+	"Employee" : {
+		"ProbaCumul Niveau de vie" : [8.6,18.7,30.4,43.8,56.9,69.2,79.7,89.1,96,100],
+		"RevenuParDecile" : [11680,13662,15644,17626,19608,21590,23572,25554,27536,29518,31500],
+		"Part dans la population" : 15.8
+	},
+	"Worker" : {
+		"ProbaCumul Niveau de vie" : [10,21.4,34.1,46.4,59.1,71.8,83.6,92.4,98,100],
+		"RevenuParDecile" : [11200,13017,14834,16651,18468,20285,22102,23919,25736,27553,29370],
+		"Part dans la population" : 12.0
+	},
+	"Retired" : { #Source : https://drees.solidarites-sante.gouv.fr/sites/default/files/2021-05/Fiche%2009%20-%20Le%20niveau%20de%20vie%20des%20retrait%C3%A9s.pdf
+		"ProbaCumul Niveau de vie" : [13,35,57,79,100],
+		"RevenuParDecile" : [607.75*12,970*12,1390*12,1780*12,2240*12,3860*12],
+		"Part dans la population" : 29.5
+	},
+	"Unemployed - under Unemployment benefit" : {#source : https://www.insee.fr/fr/statistiques/7456885?sommaire=7456956#tableau-figure2
+		"ProbaCumul Niveau de vie" : [24.9,42,53.7,62.7,69.8,76.2,82,87.5,92.9,100],
+		"RevenuParDecile" : [607.75*12,12000,12787,13573,14360,15147,15933,16720,17507,18293,19080],
+		"Part dans la population" : 7.1 #Chiffre du chomage
+	},
+	"Unemployed - under Solidarity income benefit" : {#source : https://drees.solidarites-sante.gouv.fr/sites/default/files/2022-12/AAS22-Fiche%2033%20-%20Les%20b%C3%A9n%C3%A9ficiaires%20du%20revenu%20de%20solidarit%C3%A9%20active%20%28RSA%29.pdf
+		"ProbaCumul Niveau de vie" : [100.0],
+		"RevenuParDecile" : [607.75*12,607.75*12],
+		"Part dans la population" : 6.0
+	},
 }
-
+var jobList = jobStats.keys()
 
 #################################################### 
 # FUNCTIONS
 #################################################### 
 
+func createCitizen():
+	var _name = pickName()
+	var _age = pickAge()
+	var _occupation = pickJob(_age)
+	var _revenue = getRevenue(_occupation)
+	
+	var profile : Dictionary = {
+		"Name" : _name,
+		"Age" : _age,
+		"Occupation" : _occupation,
+		"Monthly income (€)" : int(_revenue/12)
+	}
+	return profile
+
 func pickName():
 	var myFirstName = first_names.pick_random()
-	first_names.remove_at(first_names.find(myFirstName))
+	first_names.erase(myFirstName)
 	var myLastName = last_names.pick_random()
-	last_names.remove_at(last_names.find(myLastName))
+	last_names.erase(myLastName)
 	return myFirstName+" "+myLastName
 
 func pickAge():
@@ -74,6 +123,39 @@ func pickAge():
 		if dice < agePyramid["ProbaCumul"][i] : 
 			age = randi_range(agePyramid["Age"][i-1],agePyramid["Age"][i])
 			cont = false
-		else :
-			i += 1
+		else : i += 1
 	return age
+
+func pickJob(myAge):
+	var myJob : String
+	if myAge > 65 : myJob = "Retired"
+	else : 
+		var p = 0.0
+		var probabilityJob : Array = []
+		var myJobList = jobList
+		myJobList.erase("Retired")
+		for j in jobList :
+			p += jobStats[j]["Part dans la population"]
+			probabilityJob.append(p)
+		var dice = randf_range(0.0,probabilityJob[-1])
+		var i = 0
+		var cont = true
+		while cont:
+			if dice < probabilityJob[i]:
+				myJob = myJobList[i]
+				cont = false
+			else : i+=1
+	return myJob
+
+func getRevenue(myOccupation):
+	var dice = randf_range(0.0,100.0)
+	var i = 0
+	var cont = true
+	var myRevenue : float
+	while cont:
+		if dice < jobStats[myOccupation]["ProbaCumul Niveau de vie"][i] :
+			myRevenue = randf_range(jobStats[myOccupation]["RevenuParDecile"][i],jobStats[myOccupation]["RevenuParDecile"][i+1])
+			cont = false
+		else : i+=1
+	return myRevenue
+	
